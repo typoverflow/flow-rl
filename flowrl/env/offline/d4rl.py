8f2f8c60d028285d492b4b81b696b849de4e6833
typoverflow <typoverflow@outlook.com>
2025-04-12 15:41:08 +0800

    fix: import error

--- flowrl/env/offline/d4rl.py
+++ flowrl/env/offline/d4rl.py
@@ -2,7 +2,7 @@ import d4rl
 import gymnasium
 import numpy as np
 
-from flowrl.envs.gym_wrappers import EpisodeMonitor
+from flowrl.env.gym_wrappers import EpisodeMonitor
 
 
 def make_env(env_name):
@@ -10,5 +10,3 @@ def make_env(env_name):
     env = gymnasium.make('GymV21Environment-v0', env_id=env_name)
     env = EpisodeMonitor(env)
     return env
-
-
