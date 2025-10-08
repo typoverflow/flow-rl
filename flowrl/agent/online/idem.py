fa3cb0dfb296e260938631e25058e1d5f17c3fa4
typoverflow <typoverflow@outlook.com>
2025-10-08 16:45:28 -0400

    update: some logging

--- flowrl/agent/online/idem.py
+++ flowrl/agent/online/idem.py
@@ -64,6 +64,11 @@ def jit_update_idem_actor(
         loss = ((eps_pred - eps_estimation) ** 2).mean()
         return loss, {
             "loss/actor_loss": loss,
+            "misc/eps_estimation_l1": jnp.abs(eps_estimation).mean(),
+            "misc/weights": weight.mean(),
+            "misc/weight_std": weight.std(0).mean(),
+            "misc/weight_max": weight.max(0).mean(),
+            "misc/weight_min": weight.min(0).mean(),
         }
 
     new_actor, actor_metrics = actor.apply_gradient(actor_loss_fn)
